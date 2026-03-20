35654d5f4b4349e2286ea72167a072dbb885eac9
Sahil Bhavsar <zackbhavsar1209@gmail.com>
2026-03-20 19:01:23 +0530

    Removed Unused Using Statements

--- Infrastructure/Messaging/RabbitMqConsumer.cs
+++ Infrastructure/Messaging/RabbitMqConsumer.cs
@@ -5,13 +5,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace Infrastructure.Messaging
 {
